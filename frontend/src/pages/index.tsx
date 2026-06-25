848f63dac4c3aaf25fe5e4e269f4ff6cb3f6cc65
Daniel Ventura de Almeida <danielvalmeida91@gmail.com>
2026-06-25 18:53:39 -0300

    feat: adicao da pagina perfil

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,21 +1,25 @@
 import { api } from "@/api/axiosInstance";
-import { AxiosError } from "axios";
+import { AxiosError, AxiosResponse } from "axios";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Home() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const route = useRouter()
+
   const handlePerfil = async () => {
- try {
-      const response =  await api.get('/perfil')
+    try {
+      const response = await api.get('/perfil')
       console.log('response', response)
+      route.push('/perfil')
     } catch (error) {
-      if(error instanceof AxiosError) {
+      if (error instanceof AxiosError) {
         return console.warn(error.response?.data?.message)
       }
     }
-    
+
   }
 
   const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
@@ -24,10 +28,14 @@ export default function Home() {
     console.log('password', password)
 
     try {
-      const response =  await api.post('/login', { email, password })
-      console.log('response', response)
+      const response: AxiosResponse<{ auth: boolean, token: string }> = await api.post('/login', { email, password })
+
+      if (response.data.auth) {
+        const token = response.data.token
+        localStorage.setItem('user_token', token)
+      }
     } catch (error) {
-      if(error instanceof AxiosError) {
+      if (error instanceof AxiosError) {
         return console.warn(error.response?.data?.message)
       }
     }
@@ -36,7 +44,7 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen w-full">
+      <div className="flex flex-col gap-10 items-center justify-center min-h-screen w-full">
         <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center max-w-2xl">
           <div className="flex flex-col mb-8">
             <label htmlFor="email">E-mail</label>
@@ -51,7 +59,7 @@ export default function Home() {
           </div>
         </form>
         <div className="flex">
-          <button className="p-4 w-full rounded-lg bg-sky-900 hover:bg-sky-600 transition text-gray-200 text-lg" onClick={handlePerfil}>Enviar</button>
+          <button className="p-4 w-full rounded-lg bg-sky-900 hover:bg-sky-600 transition text-gray-200 text-lg" onClick={handlePerfil}>Rota /perfil</button>
 
         </div>
       </div>
